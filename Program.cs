83c0c123f0b0e3f95a96fc2294b6744069d853e1
Marcello-Cruz <marcellinhopastelzinho@gmail.com>
2022-06-24 21:45:32 -0300

    add purple color

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿console.writeline ("Sakura, Sakura" foreground color = ConsoleColor.Purple);
+console.writeline ("なぜ こ なに こ らく なに ました？" foreground color = ConsoleColor.Purple);
+console.writeline ("Pétalas caem por onde cê passa" foreground color = ConsoleColor.Purple);
+console.writeline ("助けさせてください" foreground color = ConsoleColor.Purple);
+
+console.readline ();
+
+console.writeline ("Sakura, Sakura" foreground color = ConsoleColor.Purple);
+console.writeline ("なぜ こ なに こ らく なに ました？" foreground color = ConsoleColor.Purple);
+console.writeline ("Pétalas caem por onde cê passa" foreground color = ConsoleColor.Purple);
+console.writeline ("助けさせてください" foreground color = ConsoleColor.Purple);
+
+console.readline ();
+
+console.writeline ("Queria que o tempo curasse toda a ferida");
+console.writeline ("Queria que a vida fosse um pouco mais fácil");
+console.writeline ("Faço tudo que eu posso pra não deixar a cor sumir");
+console.writeline ("Mas seu rosa virou cinza e cê perdeu o amor por mim");
+console.writeline ("Queria que o tempo curasse toda a ferida");
+console.writeline ("Queria que a vida fosse um pouco mais fácil");
+console.writeline ("Faço tudo que eu posso pra não deixar a cor sumir");
+console.writeline ("Mas seu rosa virou cinza e cê perdeu o amor por mim");
+console.writeline ("Mina, no seu rosto");
+console.writeline ("Através das lágrimas, percebo");
+console.writeline ("Tá claro pra todo mundo");
+console.writeline ("Eu e você não nascemos pra tá colado um no outro");
+console.writeline ("E por mais que dê desgosto");
+console.writeline ("Eu entendo agora as lágrimas em teu rosto");
+
+console.readline ();
+
+console.writeline ("Sakura, Sakura" foreground color = ConsoleColor.Purple);
+console.writeline ("なぜ こ なに こ らく なに ました？" foreground color = ConsoleColor.Purple);
+console.writeline ("Pétalas caem por onde cê passa" foreground color = ConsoleColor.Purple);
+console.writeline ("助けさせてください" foreground color = ConsoleColor.Purple);
+console.writeline ("Sakura, Sakura" foreground color = ConsoleColor.Purple);
+console.writeline ("なぜ こ なに こ らく なに ました？" foreground color = ConsoleColor.Purple);
+console.writeline ("Pétalas caem por onde cê passa" foreground color = ConsoleColor.Purple);
+console.writeline ("助けさせてください" foreground color = ConsoleColor.Purple);
+console.writeline ("Wow, te dei tudo que cê quer" foreground color = ConsoleColor.Purple);
+console.writeline ("Mas cê nunca tá feliz" foreground color = ConsoleColor.Purple);
+console.writeline ("Cê sempre diz que eu sou tudo que cê sempre quis" foreground color = ConsoleColor.Purple);
+console.writeline ("Wow, te dei tudo que cê quer" foreground color = ConsoleColor.Purple);
+console.writeline ("Mas cê nunca tá feliz" foreground color = ConsoleColor.Purple);
+console.writeline ("Cê sempre diz que eu sou tudo que cê sempre quis" foreground color = ConsoleColor.Purple);
+console.writeline ("Então, como é que falta" foreground color = ConsoleColor.Purple);
+console.writeline ("Tanta coisa na tua volta?" foreground color = ConsoleColor.Purple);
+console.writeline ("Cê nunca que me adora" foreground color = ConsoleColor.Purple);
+console.writeline ("E não me ama como eu te amei" foreground color = ConsoleColor.Purple);
+console.writeline ("Então, como é que passa" foreground color = ConsoleColor.Purple);
+console.writeline ("Essa dor que me assola?" foreground color = ConsoleColor.Purple);
+console.writeline ("Na escola, cê falava que a gente era pra sempre" foreground color = ConsoleColor.Purple);
+console.writeline ("Sakura, Sakura" foreground color = ConsoleColor.Purple);
+console.writeline ("なぜ こ なに こ らく なに ました？" foreground color = ConsoleColor.Purple);
+console.writeline ("Pétalas caem por onde cê passa" foreground color = ConsoleColor.Purple);
+console.writeline ("助けさせてください" foreground color = ConsoleColor.Purple);
+console.writeline ("Sakura, Sakura" foreground color = ConsoleColor.Purple);
+console.writeline ("なぜ こ なに こ らく なに ました？" foreground color = ConsoleColor.Purple);
+console.writeline ("Pétalas caem por onde cê passa" foreground color = ConsoleColor.Purple);
+console.writeline ("助けさせてください" foreground color = ConsoleColor.Purple);
